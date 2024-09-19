eb07d96ba4f4b9fe3c3b440194fc6282ba6a232c
Slavik-Bilyk <Slavataschor@ukr.net>
2024-09-19 13:53:51 +0300

    test4

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/registration'>
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
